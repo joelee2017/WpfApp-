9012d568e2e2fb18d6a1b5af982bbfaf564b21b1
joelee <as955133@gmail.com>
2018-01-07 11:53:32 +0800

    秒針、分針、時針加入 INotifyPropertyChanged 介面 通知用戶端已變更屬性值。

---
+++ WpfApp機械時鐘/MechanicalclockClass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp機械時鐘
+{
+    class MechanicalclockClass : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public int SecondAngle { get; private set; }
+        public int MinuteAngle { get; private set; }
+        public int HourAngle { get; private set; }
+
+
+    }
+}
